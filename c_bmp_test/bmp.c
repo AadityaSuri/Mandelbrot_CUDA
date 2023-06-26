603f8d1095be57b0cac43699e6d68c2510934e51
Aaditya Suri <aadityasuri1@gmail.com>
2023-06-26 14:47:29 -0700

    color grading pattern changed

--- c_bmp_test/bmp.c
+++ c_bmp_test/bmp.c
@@ -38,9 +38,110 @@ int main ()
             int b = 0;
 
             if (iteration < max_iteration) {
-                r = iteration % 256;
-                g = iteration % 256;
-                b = iteration % 256;
+                int i = iteration % 16;
+                switch (i)
+                {
+                case 0:
+                    r = 60;
+                    g = 30;
+                    b = 15;
+                    break;
+
+                case 1:
+                    r = 25;
+                    g = 7;
+                    b = 26;
+                    break;
+                
+                case 2:
+                    r = 9;
+                    g = 1;
+                    b = 47;
+                    break;
+
+                case 3:
+                    r = 4;
+                    g = 4;
+                    b = 73;
+                    break;
+                
+                case 4:
+                    r = 0;
+                    g = 7;
+                    b = 100;
+                    break;
+
+                case 5:
+                    r = 12;
+                    g = 44;
+                    b = 138;
+                    break;
+                
+                case 6:
+                    r = 24;
+                    g = 82;
+                    b = 177;
+                    break;
+
+                case 7:
+                    r = 57;
+                    g = 125;
+                    b = 209;
+                    break;
+
+                case 8:
+                    r = 134;
+                    g = 181;
+                    b = 229;
+                    break;
+
+                case 9:
+                    r = 211;
+                    g = 236;
+                    b = 248;
+                    break;
+                
+                case 10:
+                    r = 241;
+                    g = 233;
+                    b = 191;
+                    break;
+                
+                case 11:
+                    r = 248;
+                    g = 201;
+                    b = 95;
+                    break;
+
+                case 12:
+                    r = 255;
+                    g = 170;
+                    b = 0;
+                    break; 
+                
+                case 13:
+                    r = 204;
+                    g = 128;
+                    b = 0;
+                    break;
+                
+                case 14:
+                    r = 153;
+                    g = 87;
+                    b = 0;
+                    break;
+                
+                case 15:
+                    r = 106;
+                    g = 52;
+                    b = 3;
+                    break;  
+                
+                default:
+                    break;
+                }
+                
+
             }
 
             writePixel((unsigned char*) image, height, width, i, j, r, g, b);
